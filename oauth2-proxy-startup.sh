2d72224ea6002b97d1c06269727623360d84a3bf
Brad Seefeld <bradley.seefeld@gmail.com>
2016-12-23 17:07:05 -0800

    Bind to port 3000 instead

--- oauth2-proxy-startup.sh
+++ oauth2-proxy-startup.sh
@@ -1,1 +1,1 @@
-./oauth2_proxy --email-domain=$OAUTH2_EMAIL_DOMAIN --upstream=$OAUTH2_PROXY_UPSTREAM --http-address=http://0.0.0.0:4180 --cookie-secure=true
+./oauth2_proxy --email-domain=$OAUTH2_EMAIL_DOMAIN --upstream=$OAUTH2_PROXY_UPSTREAM --http-address=http://0.0.0.0:3000 --cookie-secure=true
